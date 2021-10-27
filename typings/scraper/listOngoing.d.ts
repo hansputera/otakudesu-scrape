82522a4ce76c82f1a2cfe0dbf25f236ec2d38bd5
hansputera <hanif@tslab.site>
2021-10-27 13:59:33 +0700

    chore: build files

--- typings/scraper/listOngoing.d.ts
+++ typings/scraper/listOngoing.d.ts
@@ -1,3 +1,3 @@
 import { TinyHttpClient } from 'hanif-tiny-http';
-import { OngoingAnime } from '../types';
+import type { OngoingAnime } from '../types';
 export declare const getOngoingList: (request: TinyHttpClient) => Promise<OngoingAnime[]>;
