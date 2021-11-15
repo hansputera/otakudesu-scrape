ed257cbe602a322bd8f8c314e0997b0917d1eabd
hansputera <hanif@tslab.site>
2021-11-15 15:11:47 +0700

    feat(trycatch): add trycatch exception to getStream method

--- src/scraper/getStream.ts
+++ src/scraper/getStream.ts
@@ -1,28 +1,33 @@
 import {load} from 'cheerio';
 import {tinyHttp, TinyHttpClient} from 'hanif-tiny-http';
 import type {PassThrough} from 'node:stream';
-import {OtakUtil} from '..';
+import {OtakUtil, signal} from '..';
 
 export const getAnimeStream = async (req: TinyHttpClient, url: string):
 Promise<PassThrough | undefined> => {
-  if (!OtakUtil.validateDownloadUrl(url)) return undefined;
-  else if (/batch/gi.test(url)) {
-    throw new TypeError('Batch video isn\'t supported on this method');
+  try {
+    if (!OtakUtil.validateDownloadUrl(url)) return undefined;
+    else if (/batch/gi.test(url)) {
+      throw new TypeError('Batch video isn\'t supported on this method');
+    }
+
+    const response = await req.get(url);
+
+    const $ = load(response.getContent());
+    const desuStream = $(
+        '.player-embed > .responsive-embed-stream > iframe',
+    ).attr('src');
+
+    // getting desu video
+    const responseDesu = await tinyHttp.get(desuStream as string);
+    const $desu = load(responseDesu.getContent());
+    const aaa = await tinyHttp.get(
+        $desu('#mediaplayer > source').attr('src') as string, {
+          stream: true,
+        });
+    return aaa.stream;
+  } catch (err) {
+    signal.emit('error', err);
+    return undefined;
   }
-
-  const response = await req.get(url);
-
-  const $ = load(response.getContent());
-  const desuStream = $(
-      '.player-embed > .responsive-embed-stream > iframe',
-  ).attr('src');
-
-  // getting desu video
-  const responseDesu = await tinyHttp.get(desuStream as string);
-  const $desu = load(responseDesu.getContent());
-  const aaa = await tinyHttp.get(
-      $desu('#mediaplayer > source').attr('src') as string, {
-        stream: true,
-      });
-  return aaa.stream;
 };
