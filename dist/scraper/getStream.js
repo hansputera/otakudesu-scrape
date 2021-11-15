f20929182eae9fc4b05a50ff28f3e4f6d2a7b924
hansputera <hanif@tslab.site>
2021-11-15 15:12:35 +0700

    chore(version): change package version

--- dist/scraper/getStream.js
+++ dist/scraper/getStream.js
@@ -14,20 +14,26 @@ const cheerio_1 = require("cheerio");
 const hanif_tiny_http_1 = require("hanif-tiny-http");
 const __1 = require("..");
 const getAnimeStream = (req, url) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!__1.OtakUtil.validateDownloadUrl(url))
-        return undefined;
-    else if (/batch/gi.test(url)) {
-        throw new TypeError('Batch video isn\'t supported on this method');
+    try {
+        if (!__1.OtakUtil.validateDownloadUrl(url))
+            return undefined;
+        else if (/batch/gi.test(url)) {
+            throw new TypeError('Batch video isn\'t supported on this method');
+        }
+        const response = yield req.get(url);
+        const $ = (0, cheerio_1.load)(response.getContent());
+        const desuStream = $('.player-embed > .responsive-embed-stream > iframe').attr('src');
+        // getting desu video
+        const responseDesu = yield hanif_tiny_http_1.tinyHttp.get(desuStream);
+        const $desu = (0, cheerio_1.load)(responseDesu.getContent());
+        const aaa = yield hanif_tiny_http_1.tinyHttp.get($desu('#mediaplayer > source').attr('src'), {
+            stream: true,
+        });
+        return aaa.stream;
     }
-    const response = yield req.get(url);
-    const $ = (0, cheerio_1.load)(response.getContent());
-    const desuStream = $('.player-embed > .responsive-embed-stream > iframe').attr('src');
-    // getting desu video
-    const responseDesu = yield hanif_tiny_http_1.tinyHttp.get(desuStream);
-    const $desu = (0, cheerio_1.load)(responseDesu.getContent());
-    const aaa = yield hanif_tiny_http_1.tinyHttp.get($desu('#mediaplayer > source').attr('src'), {
-        stream: true,
-    });
-    return aaa.stream;
+    catch (err) {
+        __1.signal.emit('error', err);
+        return undefined;
+    }
 });
 exports.getAnimeStream = getAnimeStream;
